506ce6474fa1be50db1264c18605216590fb572e
Aleksandr Morozov <57901166+morozov1982@users.noreply.github.com>
2021-05-11 09:51:09 +0600

    Delete ___test/react-way-of-samurai directory

--- ___test/react-way-of-samurai/src/components/Dialogs/Message/Message.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import stl from "./../Dialogs.module.css";
-
-const Message = props => {
-  return <div className={stl.message}>{props.message}</div>;
-};
-
-export default Message;
--- ___test/react-way-of-samurai/src/components/common/Preloader/Preloader.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import preloader from '../../../assets/images/preloader.gif';
-
-let Preloader = (props) => {
-    return <img src={ preloader } alt="loading..." />
-}
-
-export default Preloader;
